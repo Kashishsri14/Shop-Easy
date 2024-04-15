e3fd3bc873b7e903f9e762647a943faec581b406
kashish <ks1410ava@gmail.com>
2024-04-16 01:36:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import './App.css';
+// import Header from './Header';
+// import Home from "./Home";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// function App() {
+//   return (
+//     // BEM
+//     <Router>
+//           <div className="App">
+//       <Switch>
+//         <Route path ="/checkout">
+//         <Header />
+//         <h1>I AM A CHECKOUT PAGE</h1>
+//         </Route>
+//         <Route path="/">
+//         <Header />
+//          <Home />
+//         </Route>
+//       </Switch>
+//        </div>
+//     </Router>
+//      );
+// }
+
+// export default App;
+
+
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from './Login';
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import Payment from "./Payment";
+
 
 function App() {
+ const [{}, dispatch] = useStateValue();
+  useEffect(() =>{
+    // will only run once when the app component loads
+    auth.onAuthStateChanged(authUser =>{
+      if(authUser){
+        // the user has just logged in/ was logged in
+
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }
+      else{
+        // the user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // BEM
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/checkout" element={[<Header />, <Checkout />]}/>
+          <Route path="/" element={[<Header />, <Home />]}/>
+          <Route path="/payment" element={[<Header />, <Payment /> ]}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+const firebaseConfig = {
+    apiKey: "AIzaSyBi2dwDfOKn4nwBzOF5hgWIWcsaHxvz2ZQ",
+    authDomain: "challenge-75ee6.firebaseapp.com",
+    projectId: "challenge-75ee6",
+    storageBucket: "challenge-75ee6.appspot.com",
+    messagingSenderId: "336831562815",
+    appId: "1:336831562815:web:5e54bd993f64f17a64248a",
+    measurementId: "G-PBZGYS61LF"
+}; 
+const firebaseApp =  firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {db,auth};
